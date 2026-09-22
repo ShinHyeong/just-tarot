2777912811abb6f9d6348fa6eb43f3c0418ae564
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2026-09-22 19:46:57 +0900

    ⚙️ chore(Config) : AiModelProperties 빈으로 등록

--- src/main/java/com/justtarot/tarot_reading/TarotReadingApplication.java
+++ src/main/java/com/justtarot/tarot_reading/TarotReadingApplication.java
@@ -2,8 +2,10 @@ package com.justtarot.tarot_reading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class TarotReadingApplication {
 
 	public static void main(String[] args) {
