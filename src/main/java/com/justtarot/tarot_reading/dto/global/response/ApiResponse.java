d17fcc9ddaf520504f3846586bb5712c5433dd94
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2026-09-22 19:42:06 +0900

    ♻️ refactor(http response): http 공통 응답 DTO 추가

---
+++ src/main/java/com/justtarot/tarot_reading/dto/global/response/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.justtarot.tarot_reading.dto.global.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ApiResponse<T>(
+        int status,
+        String message,
+        T data
+) {
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(StatusCode.SUCCESS.getCode(), null, data);
+    }
+
+    public static ApiResponse<Void> success() {
+        return new ApiResponse<>(StatusCode.SUCCESS.getCode(), null, null);
+    }
+
+    public static ApiResponse<Void> created() {
+        return new ApiResponse<>(StatusCode.CREATED.getCode(), null, null);
+    }
+
+    public static ApiResponse<Void> fail(StatusCode statusCode) {
+        return new ApiResponse<>(statusCode.getCode(), statusCode.getMessage(), null);
+    }
+}
